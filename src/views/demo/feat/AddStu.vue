8c7275f144e1a5b2dd1f9821206b0eed50da807b
h-sina <120081981@qq.com>
2022-06-18 13:02:49 +0800

    v1.8.2
    删除无用的部分

--- src/views/demo/feat/AddStu.vue
+++ src/views/demo/feat/AddStu.vue
@@ -14,7 +14,7 @@
 
 <script>
 import { onMounted, reactive, toRefs, ref } from 'vue';
-import { ImpExcel } from '/@/components/Excel';
+// import { ImpExcel } from '/@/components/Excel';
 import { addStuByExcel } from '/@/api/sys/upload';
 import {
   PicRightOutlined,
@@ -24,7 +24,7 @@ import {
 } from '@ant-design/icons-vue';
 import { useMessage } from '/@/hooks/web/useMessage';
 const { notification } = useMessage();
-import { jsonToSheetXlsx, aoaToSheetXlsx } from '/@/components/Excel';
+// import { jsonToSheetXlsx, aoaToSheetXlsx } from '/@/components/Excel';
 import axios from 'axios';
 import { getToken } from '/@/utils/auth';
 
@@ -34,7 +34,7 @@ export default {
     CheckCircleTwoTone,
     CloseCircleTwoTone,
     PlusCircleTwoTone,
-    ImpExcel,
+    // ImpExcel,
   },
   setup () {
     onMounted(() => { });
@@ -43,45 +43,6 @@ export default {
       examId: '',
       href: '',
     });
-    function loadDataSuccess (excelDataList) {
-      console.log(excelDataList);
-      // var fileInput = document.querySelector('#fileSelecter');
-      // var file = fileInput.files.item(0);
-      // console.log(file);
-      let formData = new FormData();
-      formData.append('file', excelDataList);
-      console.log(formData);
-
-      const token = getToken();
-      console.log(token);
-      let config = {
-        headers: {
-          // 'content-type': 'multipart/form-data',
-          Authorization: token,
-        },
-        responseType: 'blob',
-      };
-      axios
-        .post('https://www.itestwithme.top/user/s-admin/add/student', formData, config)
-        .then((response) => {
-          // 处理后端返回的二进制流文件触发其自动下载
-          let filename = 'stu.xlsx';
-          var blob = new Blob([response.data], {
-            type: 'application/octet-stream;charset=utf-8',
-          });
-
-          let DownloadLink = document.createElement('a');
-          DownloadLink.style = 'display: none'; // 创建一个隐藏的a标签
-          DownloadLink.download = filename;
-          DownloadLink.href = window.URL.createObjectURL(blob);
-          document.body.appendChild(DownloadLink);
-          DownloadLink.click(); // 触发a标签的click事件
-          document.body.removeChild(DownloadLink);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
 
     async function uploadF () {
       var fileInput = document.querySelector('#fileSelecter');
@@ -124,7 +85,6 @@ export default {
 
     return {
       ...toRefs(data),
-      loadDataSuccess,
       uploadF,
     };
   },
